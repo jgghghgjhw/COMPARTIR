83bfbe5d9484181bce3984a0657730e7c2519a53
reginareginachavez-create <reginareginachavez@gmail.com>
2026-05-11 10:07:39 -0600

    Hola Mundo

--- src/conexion/RENE.java
+++ src/conexion/RENE.java
@@ -4,6 +4,9 @@ public class RENE {
 	
 	public static void main(String [] cecyto) {
 		System.out.print("HOLA REGINA");
+		System.out.print("Hola Jesus");
+		System.out.print("BUENOS DIAS");
 	}
+	
 
 }
