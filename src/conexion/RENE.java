7ac96d692653062648a5762f105c7227d9fdc16a
Jesus <jesusalcecyto234@gmail.com>
2026-05-11 10:10:39 -0600

    jesus hola

--- src/conexion/RENE.java
+++ src/conexion/RENE.java
@@ -5,6 +5,10 @@ public class RENE {
 	public static void main(String [] cecyto) {
 		System.out.print("HOLA REGINA");
 		System.out.print("HOLA ");
+		
+		
+		
+		System.out.print("HOLA JESUS ");
 	}
 
 }
